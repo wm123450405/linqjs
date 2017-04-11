7a1d58795aadd47070ce9e48bcea2f3b7e944e7e
wangm <wangm@samples.cn>
2017-04-11 15:01:27 +0800

    2.1.17

--- src/components/see-link.vue
+++ src/components/see-link.vue
@@ -1,10 +1,10 @@
 <template>
     <span>
-        <lang-link v-if="see.guide" :to="`guides/${ see.guide }`">{{ title || see.title }}</lang-link>
-        <lang-link v-if="!see.guide && see.apis" :to="`apis/${ see.apis }` + (see.property ? `/property/${ see.property }` : see.method ? `/method/${ see.method }` + (typeof see.index !== 'undefined' ? `/${ see.index }` : '') : '')">{{ title || (see.apis + (see.property ? '.' + see.property : see.method ? '.' + see.method : '')) }}</lang-link>
-        <mark-to v-if="!see.guide && !see.apis && see.mark" :to="see.href">{{ title || see.title }}</mark-to>
-        <lang-link v-if="!see.guide && !see.apis && !see.mark && see.inner" :to="see.href">{{ title || see.title }}</lang-link>
-        <a v-if="!see.guide && !see.mark && !see.inner && !see.apis && see.href" target="_blank" :href="outerLink(see.href)">{{ title || see.title }}</a>
+        <lang-link v-if="see.guide" :to="`guides/${ see.guide }`">{{ subject }}</lang-link>
+        <lang-link v-if="!see.guide && see.apis" :to="`apis/${ see.apis }` + (see.property ? `/property/${ see.property }` : see.method ? `/method/${ see.method }` + (typeof see.index !== 'undefined' ? `/${ see.index }` : '') : '')">{{ subject || (see.apis + (see.property ? '.' + see.property : see.method ? '.' + see.method : '')) }}</lang-link>
+        <mark-to v-if="!see.guide && !see.apis && see.mark" :to="see.href">{{ subject }}</mark-to>
+        <lang-link v-if="!see.guide && !see.apis && !see.mark && see.inner" :to="see.href">{{ subject }}</lang-link>
+        <a v-if="!see.guide && !see.mark && !see.inner && !see.apis && see.href" target="_blank" :href="outerLink(see.href)">{{ subject }}</a>
     </span>
 </template>
 <script>
@@ -13,10 +13,24 @@
     		see: Object,
             title: String
         },
+        data() {
+    		return {
+    			directory: []
+            };
+        },
+        computed: {
+    		subject() {
+				if (this.directory && this.see.guide && !this.see.title) {
+					return Enumerable.firstOrDefault(Enumerable.firstOrDefault(this.directory, { children: [] }, node => node.code === 'guides').children, { title: '' }, node => node.code === this.see.guide).title || this.see.guide || this.title;
+				} else {
+					return this.title || this.see.title || this.see.guide || '';
+                }
+            }
+        },
         mounted() {
     		if (this.see.guide && !this.see.title) {
                 this.getJson(['directory']).then(directory => {
-					this.title = Enumerable.firstOrDefault(Enumerable.firstOrDefault(directory, { children: [] }, node => node.code === 'guides').children, { title: '' }, node => node.code === this.see.guide).title || this.see.guide;
+                	this.directory = directory;
                 });
             }
         },
