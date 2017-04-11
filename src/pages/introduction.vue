e616e6c2ad88d9546dcada0d5d0170e747989247
wangm <wangm@samples.cn>
2017-04-11 16:58:51 +0800

    shields

--- src/pages/introduction.vue
+++ src/pages/introduction.vue
@@ -1,5 +1,6 @@
 <template>
     <content-template :title="data.title">
+        <a v-for="shield in data.shields" :href="shield.href"><img :src="shield.image" /></a>
         <p v-for="content in data.contents" v-html="capitalize(content)"></p>
     </content-template>
 </template>
