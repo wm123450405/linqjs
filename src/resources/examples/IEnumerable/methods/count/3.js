de864b2fb7edaa39888bde536d7879b3564870d2
wangm <wangm@samples.cn>
2017-08-28 14:41:02 +0800

    count examples

---
+++ src/resources/examples/IEnumerable/methods/count/3.js
@@ -0,0 +1,13 @@
+let pets = [
+    { name: "Barley", vaccinated: true },
+    { name: "Boots", vaccinated: false },
+    { name: "Whiskers", vaccinated: false }
+];
+
+let numberUnvaccinated = pets.asEnumerable().count({ vaccinated: false });
+
+console.log(`There are ${ numberUnvaccinated } unvaccinated animals.`);
+
+// This code produces the following output:
+//
+// There are 2 unvaccinated animals.
