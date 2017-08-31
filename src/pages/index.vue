3acabc5eb4b7ee14c61e8bdbf21776fb3860abfe
wangm <wangm@samples.cn>
2017-08-31 17:51:26 +0800

    editor

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -7,10 +7,50 @@
 			<router-view name="directory" class="col-sm-4 col-lg-3 sidebar" id="sidebar"></router-view>
 			<router-view name="content" class="col-sm-8 col-lg-9" style="padding-bottom: 5em;" id="content"></router-view>
 		</div>
+		<div class="try">
+			<textarea title="javascript code" id="code" class="code"></textarea>
+			<div class="result">
+				<div class="btns">
+					<div @click="tryIt"><i class="fa fa-fw fa-play"></i>运行</div>
+					<div><i class="fa fa-fw fa-clear"></i>清空</div>
+					<div class="expand"><i class="fa fa-fw fa-expand"></i>打开</div>
+				</div>
+				<ul class="list">
+					<li v-for="logs in logList"><span v-for="log in logs"> {{ log | json }} </span></li>
+				</ul>
+			</div>
+		</div>
 	</div>
 </template>
 <script>
-	export default {
+	const CodeMirror = require('codemirror');
 
+	let codeMirror;
+
+	export default {
+	    data() {
+	        return {
+                logList: []
+			}
+		},
+		mounted() {
+            codeMirror = CodeMirror.fromTextArea(document.getElementById('code'), {
+                mode: 'javascript',
+                lineNumbers: true
+			});
+		},
+		methods: {
+		    tryIt() {
+		        let code = codeMirror && codeMirror.getValue() || $('#code').val();
+                alert(code);
+                const Enumerable = require('linq-js');
+		        let log = console.log;
+		        console.log = (...contents) => {
+		            this.logList.push(contents);
+				};
+				eval(code);
+				console.log = log;
+			}
+		}
 	};
 </script>
