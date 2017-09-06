60c625e5ad3e084b338e255b541e70903138bf42
wangm <wangm@samples.cn>
2017-09-06 17:45:37 +0800

    2.1.19 fixed bug

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -39,19 +39,23 @@
             codeMirror && codeMirror.setValue('');
 	        this.clear();
 	        this.close();
-            let reg = /^\/(\w+-\w+)(\/\d+\.\d+\.\d+(\.pre)?)?(.+)?$/i;
+            let reg = /^\/(\w+-\w+)(\/\d+\.\d+\.\d+(\.pre)?)?(.*)?$/i;
             to = reg.exec(to.path);
+            console.log(to);
             to = {
                 lang: to && to[1] || common.defaultLang,
 				version: to && to[2] && to[2].substring(1) || common.lastest,
-                url: to && to[4] || ''
+                url: to && to[4] || '/'
 			};
+            console.log(to);
             from = reg.exec(from.path);
+            console.log(from);
             from = {
                 lang: from && from[1] || common.defaultLang,
                 version: from && from[2] && from[2].substring(1) || common.lastest,
-                url: from && from[4] || ''
+                url: from && from[4] || '/'
             };
+            console.log(from);
 			if (to.url !== from.url) {
 			    if (to.lang === from.lang && to.version === from.version) {
 			        next();
