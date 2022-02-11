8150fff205bdde637a42fb6749fbd270c9ee711b
wangmiao <491029934@qq.com>
2022-02-11 17:42:40 +0800

    规避违禁内容

--- src/resources/zh-cn/examples/use/2.js
+++ src/resources/zh-cn/examples/use/2.js
@@ -1,4 +1,4 @@
-let xi = { name: "习大大" }, wang = { name: "王思聪" }, li = { name: "李小龙" };
+let xi = { name: "大龙哥" }, wang = { name: "王思聪" }, li = { name: "李小龙" };
 let white = { name: "小白", owner: wang }, boots = { name: "机器人", owner: wang }, black = { name: "黑蛋", owner: li }, cai = { name: "旺财", owner: xi };
 let people = [ xi, wang, li ];
 let pets = [ white, boots, black, cai ];
@@ -15,7 +15,7 @@ for (let obj of query) {
 /*
  这段代码的输出结果如下：
 
- 习大大 - 旺财
+ 大龙哥 - 旺财
  王思聪 - 小白
  王思聪 - 机器人
  李小龙 - 黑蛋
